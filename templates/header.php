7a69db119bde9c009e5e072d3b99dd822e403aa5
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-08-28 17:20:55 +0200

    consistency touchups

--- templates/header.php
+++ templates/header.php
@@ -7,13 +7,13 @@
     <meta http-equiv="refresh" content="<?= $GLOBALS['REFRESH'] * 60 ?>; URL='?dispatch=logout'">
 <? endif ?>
     <title>Stud.IP Plugin Marktplatz</title>
-    <link rel="stylesheet" type="text/css" href="<?= $GLOBALS['BASE_URI'] ?>css/newstyle/style.css" media="screen">
-    <link rel="stylesheet" type="text/css" href="<?= $GLOBALS['BASE_URI'] ?>css/newstyle/style_content.css" media="screen">
-    <link rel="stylesheet" href="<?= $BASE_URI ?>css/basis.css" type="text/css">
-    <link rel="stylesheet" href="<?= $BASE_URI ?>css/tagcloud.css" type="text/css">
-    <link rel="stylesheet" href="<?= $BASE_URI ?>css/jquery/jquery.ui.all.css" type="text/css">
-    <link rel="stylesheet" href="<?= $BASE_URI ?>css/jquery.lightbox-0.5.css" type="text/css" media="screen">
-    <link rel="stylesheet" href="<?= $BASE_URI ?>css/star_rating.css" type="text/css" media="all">
-    <link rel="stylesheet" href="<?= $BASE_URI ?>css/basis.css" type="text/css">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/newstyle/style.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/newstyle/style_content.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/basis.css" type="text/css">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/tagcloud.css" type="text/css">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/jquery/jquery.ui.all.css" type="text/css">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/jquery.lightbox-0.5.css" type="text/css" media="screen">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/star_rating.css" type="text/css" media="all">
+    <link rel="stylesheet" href="<?= $GLOBALS['BASE_URI'] ?>css/basis.css" type="text/css">
 </head>
 <body>
