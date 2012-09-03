4ec2f35e3d08ff47b9685b69a34cd44a1583ed0a
Jan-Hendrik Willms <tleilax+github@gmail.com>
2012-09-03 12:59:23 +0200

    forgot this file

--- classes/StudipAuth.class.php
+++ classes/StudipAuth.class.php
@@ -1,10 +1,12 @@
 <?php
 /**
-* @author               Jan Kulmann <jankul@zmml.uni-bremen.de>
-*/
+ * @author Jan-Hendrik Willms <tleilax+studip@gmail.com>
+ * @author Jan Kulmann <jankul@zmml.uni-bremen.de>
+ */
 
 // +---------------------------------------------------------------------------+
 // Copyright (C) 2012 Jan Kulmann <jankul@zmml.uni-bremen.de>
+// Copyright (C) 2012 Jan-Hendrik Willms <tleilax+studip@gmail.com>
 // +---------------------------------------------------------------------------+
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -23,63 +25,62 @@
 ini_set('soap.wsdl_cache_enabled', '0');
 ini_set('soap.wsdl_cache_ttl', '0');
 
-class StudipAuth {
+class StudipAuth
+{
 
-	private $client;
-	
-	public function __construct() { 
-		$this->client = new SoapClient($GLOBALS['WSDL_ENDPOINT'],
-						array("uri" => "urn:studip_wsd",                 //Ein Namespace
-						      "style" => SOAP_RPC,                               //Art der Handhabung, hier Methodenaufruf (Remote Procedure Call)
-						      "use" => SOAP_ENCODED                     //Verschlüsselte Übertragung
-						     )
-					      );
-	}
+    private $client;
+    
+    public function __construct() { 
+        $this->client = new SoapClient($GLOBALS['WSDL_ENDPOINT'],
+                        array("uri" => "urn:studip_wsd",                 //Ein Namespace
+                              "style" => SOAP_RPC,                               //Art der Handhabung, hier Methodenaufruf (Remote Procedure Call)
+                              "use" => SOAP_ENCODED                     //Verschlüsselte Übertragung
+                             )
+                          );
+    }
 
-	public function checkUserCredentials($user_name, $password) {
-		$user_name = base64_encode(CryptMP::encryptPrivate($user_name));
-		$password = base64_encode(CryptMP::encryptPrivate($password));
-		$confirmed = FALSE;
-		try {
-			$confirmed = $this->client->check_user_credentials($GLOBALS['SOAP_API_KEY'],$user_name, $password);
-		} catch (Exception $e) {
-			echo 'Exception: ',  $e->getMessage(), "\n";
-		}
-		return $confirmed;
-	}
+    public function checkUserCredentials($user_name, $password) {
+        $user_name = base64_encode(CryptMP::encryptPrivate($user_name));
+        $password = base64_encode(CryptMP::encryptPrivate($password));
+        $confirmed = FALSE;
+        try {
+            $confirmed = $this->client->check_user_credentials($GLOBALS['SOAP_API_KEY'],$user_name, $password);
+        } catch (Exception $e) {
+            echo 'Exception: ',  $e->getMessage(), "\n";
+        }
+        return $confirmed;
+    }
 
-	public function getUserInformation($user_name) {
-		$user_name = base64_encode(CryptMP::encryptPrivate($user_name));
-		$userinformation = array();
-		try {
-			$userinformation = $this->client->get_user_by_user_name($GLOBALS['SOAP_API_KEY'],$user_name);
-		} catch (Exception $e) {
-			echo 'Exception: ',  $e->getMessage(), "\n";
-		}
-		return $userinformation;
-	}
+    public function getUserInformation($user_name) {
+        $user_name = base64_encode(CryptMP::encryptPrivate($user_name));
+        $userinformation = array();
+        try {
+            $userinformation = $this->client->get_user_by_user_name($GLOBALS['SOAP_API_KEY'],$user_name);
+        } catch (Exception $e) {
+            echo 'Exception: ',  $e->getMessage(), "\n";
+        }
+        return $userinformation;
+    }
 
-	public function authenticate($user_name, $password) {
-		if (!$this->checkUserCredentials($user_name, $password)) return FALSE;
+    public function authenticate($user_name, $password) {
+        if (!$this->checkUserCredentials($user_name, $password)) return FALSE;
 
-		$cryptinformation = unserialize(base64_decode($this->getUserInformation($user_name)));
+        $cryptinformation = unserialize(base64_decode($this->getUserInformation($user_name)));
 
-		if (!count($cryptinformation)) {
-			return FALSE;
-		}
+        if (!count($cryptinformation)) {
+            return FALSE;
+        }
 
-		$first_name = trim(CryptMP::decryptPrivate(base64_decode($cryptinformation['first_name'])));
+        $first_name = trim(CryptMP::decryptPrivate(base64_decode($cryptinformation['first_name'])));
                 $last_name  = trim(CryptMP::decryptPrivate(base64_decode($cryptinformation['last_name'])));
                 $email      = trim(CryptMP::decryptPrivate(base64_decode($cryptinformation['email'])));
 
-		$userinformation = array('first_name'=>$first_name, 'last_name'=>$last_name, 'email'=>$email);
+        $userinformation = array('first_name'=>$first_name, 'last_name'=>$last_name, 'email'=>$email);
 
-		if (empty($first_name) || empty($last_name) || empty($email)) {
-			return FALSE;
-		}
-		return $userinformation;
-	}
+        if (empty($first_name) || empty($last_name) || empty($email)) {
+            return FALSE;
+        }
+        return $userinformation;
+    }
 
 }
-
-?>
